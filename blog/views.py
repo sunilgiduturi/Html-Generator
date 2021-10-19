bfa9604265e4d7be449c6673573a03a9724502e0
sunil <sunil@sunils-MacBook-Air.local>
2021-10-19 13:41:32 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+from blog.forms import PostForm
+
+# Create your views here.
+def home(request):
+    form = PostForm()
+    return render(request,'blog/home.html',{'form':form})
